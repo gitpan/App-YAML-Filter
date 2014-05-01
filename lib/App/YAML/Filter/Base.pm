4912dd538f079c9462460dbe4fb0849f2f42ac7c
Doug Bell <preaction@cpan.org>
2014-05-01 05:35:34 +0000

    Import of PREACTION/App-YAML-Filter-0.002 from CPAN.
    gitpan-cpan-distribution: App-YAML-Filter
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         PREACTION/App-YAML-Filter-0.002.tar.gz
    gitpan-cpan-author:       PREACTION
    gitpan-cpan-maturity:     released

--- lib/App/YAML/Filter/Base.pm
+++ lib/App/YAML/Filter/Base.pm
@@ -1,6 +1,6 @@
 package App::YAML::Filter::Base;
 {
-  $App::YAML::Filter::Base::VERSION = '0.001';
+  $App::YAML::Filter::Base::VERSION = '0.002';
 }
 # ABSTRACT: Base module for App::YAML::Filter
 
@@ -28,7 +28,7 @@ App::YAML::Filter::Base - Base module for App::YAML::Filter
 
 =head1 VERSION
 
-version 0.001
+version 0.002
 
 =head1 AUTHOR
 
